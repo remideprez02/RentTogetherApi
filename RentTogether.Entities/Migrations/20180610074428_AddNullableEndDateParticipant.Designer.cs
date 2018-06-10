1745bda73ca920fd7577ca1ffc8573f4e8ad03f2
Remi Deprez <remideprez02@gmail.com>
2018-06-10 09:45:09 +0200

    Add some migration

--- RentTogether.Entities/Migrations/20180610074428_AddNullableEndDateParticipant.Designer.cs
+++ RentTogether.Entities/Migrations/20180610074428_AddNullableEndDateParticipant.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace RentTogether.Entities.Migrations
 {
     [DbContext(typeof(RentTogetherDbContext))]
-    partial class RentTogetherDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180610074428_AddNullableEndDateParticipant")]
+    partial class AddNullableEndDateParticipant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RentTogether.Entities/Migrations/20180610074428_AddNullableEndDateParticipant.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RentTogether.Entities.Migrations
+{
+    public partial class AddNullableEndDateParticipant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Participants",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Participants",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
