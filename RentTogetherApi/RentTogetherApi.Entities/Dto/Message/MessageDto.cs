972a75f38efcf5a6e8224bc47203d3f2626f96ea
Remi Deprez <remideprez02@gmail.com>
2018-06-18 16:35:14 +0200

    Clean Branch Dev

--- RentTogetherApi/RentTogetherApi.Entities/Dto/Message/MessageDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace RentTogetherApi.Entities.Dto.Message
-{
-    public class MessageDto
-    {
-        public int UserId { get; set; }
-        public string MessageText { get; set; }
-    }
-}
